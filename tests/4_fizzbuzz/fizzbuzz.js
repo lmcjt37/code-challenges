d1b16b0317c5782d93bf857d4adfef2175df8141
Luke Taylor <lmcjt@outlook.com>
2021-12-01 17:06:46 +0000

    add fizzbuzz test

---
+++ tests/4_fizzbuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+/**
+ * Complete the function so that it will accept a starting value and log...
+ * 'fizz' for multiples of 3
+ * 'buzz' for multiples of 5
+ * 'fizzbuzz' for multiples of 3 and 5
+ * and the integer value for anything else
+ *
+ * Example: fizzbuzz(5)
+ * 1
+ * 2
+ * fizz
+ * 4
+ * buzz
+ */
+
+export default function fizzbuzz(value) {
+  console.log(value)
+}
